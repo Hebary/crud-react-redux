069d0e0da63ed6375a3a6b19dfab3eed263b305d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-01 08:32:44 +0800

    Delete src directory

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-//Here i import all  reducers.
-import productsReducer from "./productsReducer"
-import alertReducer from "./alertReducer"
-
-export default combineReducers({
-    products: productsReducer,
-    alert: alertReducer
-});
